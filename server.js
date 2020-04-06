ca199c9ddcaffbf77de6e773010799d51b81d4c4
fadjack <adoumfadjack@rocketmail.com>
2020-04-06 16:08:46 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+//const bookRouter = require('./routes/books')
 
 
 app.set('view engine', 'ejs')
@@ -23,14 +24,14 @@ app.use(bodyParser.urlencoded({limit:'10mb', extended: false} ))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true, useUnifiedTopology: true})
+    useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+//app.use('/books', bookRouter)
 
 
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 4000)
